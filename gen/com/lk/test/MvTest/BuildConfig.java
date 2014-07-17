4ed0c209753b3ed16a7ce547e70fddaba0adb1e5
mesith <mesith.w@aeturnum.com>
2014-07-17 11:21:00 +0530

    first commit

---
+++ gen/com/lk/test/MvTest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.lk.test.MvTest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ target/generated-sources/r/com/lk/test/MvTest/BuildConfig.java
@@ -0,0 +1,5 @@
+package com.lk.test.MvTest;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = true;
+}
